c6f4a10c7352627bdab40a8cceafba40f3065e7e
Anna Harutyunyan <anuanharutyunyan>
2022-01-14 22:56:04 +0400

    Add: a better filesystem tree

--- trees/app.js
+++ trees/app.js
@@ -5,24 +5,47 @@ class Node {
     this.parent = parentNode;
   }
   addNode(value) {
-    const node = new Node(value, this);
-    this.children.push(node);
-    return { node: node, index: this.children.length - 1 };
+    const segments = value.split("/");
+    if (segments.length === 0) {
+      return;
+    }
+    if (segments.length === 1) {
+      const node = new Node(segments[0], this);
+      this.children.push(node);
+      return { node: node, index: this.children.length - 1 };
+    }
+    const existingChildNode = this.children.find(
+      (child) => child.value === segments[0]
+    );
+    if (existingChildNode) {
+      existingChildNode.addNode(segments.slice(1).join("/"));
+    } else {
+      const node = new Node(segments[0], this);
+      node.addNode(segments.slice(1).join("/"));
+      this.children.push(node);
+      return { node: node, index: this.children.length - 1 };
+    }
   }
-  removeNode(index) {
-    this.children.splice(index, 1);
-  }
+  // removeNode(index) {
+  //   this.children.splice(index, 1);
+  // }
 }
 
 class Tree {
   constructor(rootValue) {
     this.root = new Node(rootValue);
   }
+
+  add(path) {
+    this.root.addNode(path);
+  }
+
+  remove(path) {}
 }
 const filesystem = new Tree("/");
-const documentsNodeData = filesystem.root.addNode("./documents");
-const gamesNodeData = filesystem.root.addNode("./games");
-documentsNodeData.node.addNode("results.txt");
-gamesNodeData.node.addNode("code.exe");
+
+filesystem.add("/documents/personal/tax.docx");
+filesystem.add("/games/code.exe");
+filesystem.add("/games/code2.exe");
 
 console.log(filesystem);
